2974b38e098805972a17034f750979928aff0cd7
nhoughto <nhoughto5@gmail.com>
2018-05-09 18:31:14 -0700

    Added plan doc

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,23 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import Test from '../Classes/Test';
-const IndexPage = ({data}) => (
-  <div>
+const IndexPage = ({data}) => {
+  console.log("Hello");
+return (  <div>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <h3>Test.sayHello()</h3>
+    <h3>{Test.sayHello()}</h3>
     <Link to="/page-2/">Go to page 2</Link>
     {data.allMarkdownRemark.edges.map(post => (
         <Link
             key={post.node.id}
-            href={post.node.frontmatter.path}>
+            to={post.node.frontmatter.path}>
             {post.node.frontmatter.title}
         </Link>
     ))}
-  </div>
-);
+  </div>)
+};
 
 export const pageQuery = graphql `
     query IndexQuery {
