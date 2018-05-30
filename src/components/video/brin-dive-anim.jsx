df1453336cd8964831d3ab60d735dd6ce7550f12
Nick Houghton <nhoughto5@gmail.com>
2018-05-29 19:17:24 -0700

    Update

--- src/components/video/brin-dive-anim.jsx
+++ src/components/video/brin-dive-anim.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 import styled from "styled-components";
 import BannerAnim from '../../classes/VideoBanner';
 const CanvasBanner = styled.div`
-    background-color: white;
     width: 100%;
     height: 35rem;
+    background-color:#171754;
+    background-image:
+    radial-gradient(white, rgba(255,255,255,.2) 2px, transparent 20px),
+    radial-gradient(white, rgba(255,255,255,.15) 1px, transparent 10px),
+    radial-gradient(white, rgba(255,255,255,.1) 2px, transparent 20px),
+    radial-gradient(rgba(255,255,255,.4), rgba(255,255,255,.1) 2px, transparent 30px);
+    background-size: 550px 550px, 350px 350px, 250px 250px, 150px 150px; 
+    background-position: 0 0, 40px 60px, 130px 270px, 70px 100px;
 `;
 
 class BrinDiveAnim extends React.Component {
