e99b444f381b4ab4fc69450d26350ff3ee4e9911
Nick Houghton <nhoughto5@gmail.com>
2018-06-05 20:37:56 -0700

    askdh

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,6 @@ module.exports = {
     ],    
     "rules": {
         "no-param-reassign": 0,
+        "arrow-parens": 0,
     }
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from 'gatsby-link';
+import { storage } from 'react-easy-params'
 import HomePage from '../components/home-page';
 import TitlePage from '../components/title-page';
 import { timer } from 'rxjs';
@@ -12,27 +13,46 @@ class IndexPage extends React.Component {
     this.state = {
       classes: '',
       showHome: false,
+      isFirstVisit: this.isFirstVisit
     };
   }
 
+  get isFirstVisit() {
+    return sessionStorage.getItem('shownTitle') == null;
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return !nextState.isFirstVisit;
+  }
+
   componentDidMount() {
-    // timer(4000).pipe(
-    //   tap((x) => this.setState({
-    //     classes: 'title-animation-div animationFade',
-    //     showHome: true
-    //   })),
-    //   delay(3000)
-    // ).subscribe(
-    //   () => this.setState({ classes: 'title-animation-div animationFade title-no-show' })
-    // );
+    if(this.isFirstVisit){
+      console.log("dhsdfh");
+      timer(4000).pipe(
+        tap((x) => this.setState({
+          classes: 'title-animation-div animationFade',
+          showHome: true,
+        })),
+        delay(3000),
+      ).subscribe(
+        () => {
+          sessionStorage.setItem('shownTitle', true)
+          this.setState({ 
+            classes: 'title-animation-div animationFade title-no-show',
+            isFirstVisit: this.isFirstVisit,
+          })
+        }
+      );
+    }
   }
   
   render() {
+    console.log(this.state.classes);
     return (
       <div>
-        {/* <TitlePage classes={this.state.classes} /> */}
-        <HomePage/>
-        {this.state.showHome && <HomePage/>}
+        {this.isFirstVisit && <TitlePage classes={this.state.classes} />}
+        {this.isFirstVisit && this.state.showHome && <HomePage/>}
+        {!this.isFirstVisit && <HomePage/>}
       </div>
     );
   }
