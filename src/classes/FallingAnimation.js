e115ca1755f20f600be21b0124b22582a57e2e17
nhoughto5 <nhoughto5@gmail.com>
2018-11-12 20:39:49 -0800

    Have Devon and Trav working

--- src/classes/FallingAnimation.js
+++ src/classes/FallingAnimation.js
@@ -8,6 +8,8 @@ import cloud3 from '../images/cloud3.png';
 import leaf1 from '../images/leaf1.png';
 import snowflake1 from '../images/snowflake1.png';
 import moon from '../images/Moon.png';
+import devon from '../images/DevonJump.png';
+import travis from '../images/TravisJump.png';
 
 export default class FallingAnimation {
   constructor() {
@@ -31,11 +33,45 @@ export default class FallingAnimation {
     this.addDebris(leaf1, 0.01, 5);
     this.addDebris(snowflake1, 0.1, 5);
     this.addBrin();
+    this.devonSprite = this.addSprite(devon, 0.06, 0.65, 1.1, 0);
+    this.travSprite = this.addSprite(travis, 0.075, 0.2, 0.5, 180, true);//this.container.offsetHeight + Utility.randomNumFromRange(50, 100));
     this.addCloud(cloud1);
     this.addCloud(cloud2);
     this.addCloud(cloud3);
     this.attachListeners();
     this.app.ticker.add(this.animate, this);
+  }
+
+  addSprite(image, scale = 0.05, xPercent, yPercent, startAngle, spin = false) {
+    let sprite = PIXI.Sprite.fromImage(image);
+    sprite.scale.set(-scale, scale);
+    this.stage.addChild(sprite);
+    sprite.x = this.container.offsetWidth * xPercent;
+    sprite.y = this.container.offsetHeight * yPercent;
+    sprite.currentRotAngle = startAngle;
+    sprite.fallSpeed = Utility.randomNumFromRange(0.2, 0.5);
+    sprite.startX = sprite.x;
+    sprite.startY = sprite.y;
+    sprite.spin = spin;
+    sprite.pivot.set(sprite.width / 2, sprite.height / 2);
+    return sprite;
+  }
+
+  addBrin() {
+    PIXI.loader.reset();
+    PIXI.loader.add(brin).load(() => {
+      const { texture } = PIXI.loader.resources[brin];
+      const rectangle = new PIXI.Rectangle(1082, 0, 593, 452);
+      texture.frame = rectangle;
+      this.brinSprite = new PIXI.Sprite(texture);
+      let scale = 0.7 * window.innerWidth / this.normalPageSize;
+      this.brinSprite.scale.set(scale, scale);
+      this.stage.addChild(this.brinSprite);
+      this.spriteXSpeed = 1;
+      this.spriteYSpeed = 1;
+      this.brinAngle = 0;
+      this.container.style.height = 'auto';
+    });
   }
 
   attachListeners() {
@@ -127,28 +163,27 @@ export default class FallingAnimation {
     }
   }
 
-  addBrin() {
-    PIXI.loader.reset();
-    PIXI.loader.add(brin).load(() => {
-      const { texture } = PIXI.loader.resources[brin];
-      const rectangle = new PIXI.Rectangle(1082, 0, 593, 452);
-      texture.frame = rectangle;
-      this.brinSprite = new PIXI.Sprite(texture);
-      let scale = 0.75 * window.innerWidth / this.normalPageSize;
-      this.brinSprite.scale.set(scale, scale);
-      this.stage.addChild(this.brinSprite);
-      this.brinXSpeed = 1;
-      this.brinYSpeed = 1;
-      this.brinAngle = 0;
-      this.container.style.height = 'auto';
-    });
+  moveBrin(delta) {
+    const amplitude = 25;
+    if(!this.brinSprite) return;
+    this.brinSprite.x = (this.container.offsetWidth / 2 - this.brinSprite.width / 2) + amplitude * Math.sin(this.brinAngle * 0.02);
+    this.brinSprite.y = (this.container.offsetHeight / 2 - this.brinSprite.height / 2) + amplitude * Math.sin(this.brinAngle * 0.02);
+    this.brinAngle += delta;
   }
 
-  moveBrin(delta) {
-    if(!this.brinSprite) return;
-    this.brinSprite.x = (this.container.offsetWidth / 2 - this.brinSprite.width / 2) + 40 * Math.sin(this.brinAngle * 0.02);
-    this.brinSprite.y = (this.container.offsetHeight / 2 - this.brinSprite.height / 2) + 40 * Math.sin(this.brinAngle * 0.02);
-    this.brinAngle += delta;
+  moveSprite(sprite, delta) {
+    const amplitude = 0.5;
+    const freq = 0.03;
+    let fallspeed = 0.35;
+    sprite.y -= delta * fallspeed;
+    sprite.x += 0.5 + amplitude * (Math.sin(sprite.currentRotAngle * freq));
+    sprite.currentRotAngle += delta;
+    if (sprite.spin) sprite.rotation += 0.001 * (Math.sin(freq * sprite.currentRotAngle) + 5);
+    if (sprite.y < -sprite.height) {
+      sprite.x = sprite.startX;
+      sprite.y = this.container.offsetHeight + sprite.height;
+      sprite.fallSpeed = Utility.randomNumFromRange(0.2, 0.5);
+    }
   }
 
   animate(delta) {
@@ -156,6 +191,8 @@ export default class FallingAnimation {
     this.updateMoon();
     this.updateCloud();
     this.moveBrin(delta);
+    this.moveSprite(this.travSprite, delta);
+    this.moveSprite(this.devonSprite, delta);
   }
 
   updateMoon() {
